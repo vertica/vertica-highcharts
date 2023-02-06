25c6391b29749a37aa2ed7b69734dd862d90d99d
Badr <32390048+oualib@users.noreply.github.com>
2023-02-06 09:59:44 -0600

    Initial Commit (#1)
    We use the https://github.com/kyper-data/python-highcharts folder but we replace the python-highcharts call by vertica-highcharts.
    
    We also correct the syntaxes which are no more used by newer Python version.
    
    The goal is to maintain this repo as the Kyper team stopped the maintenance of their project.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+ns = {}
+with open(os.path.join(here, 'highcharts', 'version.py')) as f:
+   exec(f.read(), {}, ns)
+
+setup(
+    name='vertica-highcharts',
+    version=ns['__version__'],
+    author='Kyper Developers',
+    author_email='developers@kyperdata.com',
+    packages=find_packages(),
+    package_data={
+        'highcharts.highcharts': ['templates/*.html'],
+        'highcharts.highmaps': ['templates/*.html'],
+        'highcharts.highstock': ['templates/*.html']
+    },
+    url='https://github.com/vertica/vertica-highcharts',
+    download_url='https://github.com/vertica/vertica-highcharts/tarball/' + ns['__version__'],
+    description='Python Highcharts wrapper',
+    install_requires=[
+        "Jinja2",
+        "future"
+    ],
+    keywords = ['python', 'ipython', 'highcharts', 'chart', 'visualization', 'graph', 'javascript', 'html'],
+    classifiers         = [
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
